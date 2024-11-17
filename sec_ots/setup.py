1dc5b923c25557c46cef0c9cacaf5321bc659995
MattcOTS <onethirtysevenllc@gmail.com>
2024-11-17 13:55:35 -0500

    requirements and package setup

---
+++ sec_ots/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sec_ots',
+    version = "0.0.1",
+    description = "A wrapper for SEC API",
+    author = "OneThirtySeven",
+    author_email = "onethirtysevenllc@gmail.com",
+    packages = find_packages(),
+    install_requires = ['']
+)
